7dad421037d84b9f52c43726825a67f8d82afff1
JuanBindez <juanbindez780@gmail.com>
2023-04-09 22:21:01 -0300

    0904232220 ----> v1.0.0-rc1 contrib: <JuanBindez>

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+# this is part of the PDFextract_text project.
+#
+# Release: v1.0.0-rc1
+#
+# Copyright (c) 2021-2023 MariyaSha
+#
+# Contrib: Juan Bindez <juanbindez780@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#  
+
+
 import tkinter as tk
 import PyPDF2
 from PIL import Image, ImageTk
